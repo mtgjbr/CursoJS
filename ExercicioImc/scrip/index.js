7d862027f61924dcd2f153ed65e824ef5e57611a
mtgjbr <almmateus030@gmail.com>
2025-05-31 15:32:26 -0300

    feat:termino de estrutura de controle

--- ExercicioImc/scrip/index.js
+++ ExercicioImc/scrip/index.js
@@ -1,26 +1,42 @@
 function Meuescopo() {
-   
-   let resultado = document.querySelector('.resultado')
 
-function recebeEventoForm(evento) {
-    evento.preventDefault();
+    let resultado = document.querySelector('.resultado')
 
-    const peso = document.querySelector('#peso');
-    const altura = document.querySelector('#altura');
-    const alturaEmM = parseFloat(altura.value);
-    const pesoEmKg =parseFloat(peso.value);
-    let imc = pesoEmKg / (alturaEmM * alturaEmM);
-   
-    if (imc<18.5){
-        resultado.innerHTML = `Seu imc é ${imc.toFixed(2)} (abaixo do peso).`;
+    function recebeEventoForm(evento) {
+        evento.preventDefault();
+
+        const peso = document.querySelector('#peso');
+        const altura = document.querySelector('#altura');
+        const alturaEmM = parseFloat(altura.value);
+        const pesoEmKg = parseFloat(peso.value);
+        let imc = pesoEmKg / (alturaEmM * alturaEmM);
+
+        if (imc < 18.5) {
+            resultado.innerHTML = `Seu imc é ${imc.toFixed(2)} (abaixo do peso).`;
+
+        }
+        else if (imc > 18.5 && imc < 24.9) {
+            resultado.innerHTML = `Seu imc é ${imc.toFixed(2)} (peso ideal)`;
+        }
+        else if (imc > 25 && imc < 29.9) {
+            resultado.innerHTML = `seu imc é ${imc.toFixed(2)} (sorbepeso)`;
+
+        }
+        else if (imc > 30 && imc < 34.9) {
+            resultado.innerHTML = `seu imc é ${imc.toFixed(2)} (obesidade grau 1)`;
         
-    }
-    else if(imc>18.5 && imc<24.9){
-        resultado.innerHTML = `Seu imc é ${imc.toFixed(2)} (peso ideal)`
-    }
+        }
+        else if (imc > 35 && imc < 39.9) {
+            resultado.innerHTML = `seu imc é ${imc.toFixed(2)} (sobesidade grau 2)`;
+        }
+        else if (imc > 40) {
+            resultado.innerHTML = `seu imc é ${imc.toFixed(2)} (obesidade grau 3)`;
+        }
+        else if (isNaN(imc) || alturaEmM<=1.00|| pesoEmKg<10){
+            resultado.innerHTML = "insira um número valido"
+        }
 
-    
-}
-  document.addEventListener('submit', recebeEventoForm);
+    }
+    document.addEventListener('submit', recebeEventoForm);
 }
 Meuescopo();
